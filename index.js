1aa34aad7ae7c9e8ff7a403698557be7fdb9ba7e
Brandon G-Home <brandon.gardner2@outlook.com>
2019-03-22 12:52:07 -0500

    Changed nodemon because of error

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require("./server");
 
 server.listen(4000, () => {
-  console.log("\n *** The server is listening on port 4000 *** \n");
+  console.log("\n *** The server is listening on port 4000! *** \n");
 });
