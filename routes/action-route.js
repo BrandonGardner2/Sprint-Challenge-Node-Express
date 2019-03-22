d30f0e9bcd556dc8aa57211ee6a338686da321d6
Brandon G-Home <brandon.gardner2@outlook.com>
2019-03-22 10:29:41 -0500

    Remove function working correctly

--- routes/action-route.js
+++ routes/action-route.js
@@ -17,7 +17,7 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  if (req.body.description && req.body.notes) {
+  if (req.body.description && req.body.notes && req.body.project_id) {
     try {
       const action = await db.insert(req.body);
       res.status(200).json(action);
@@ -29,7 +29,45 @@ router.post("/", async (req, res) => {
   } else {
     res
       .status(400)
-      .json({ message: "Please provide both a description and notes." });
+      .json({ message: "Please provide a description, id and notes." });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  if (req.body.description && req.body.notes && req.body.project_id) {
+    const { id } = req.params;
+    try {
+      const updated = await db.update(id, req.body);
+      if (count) {
+        res.status(200).json(updated);
+      } else {
+        res.status(404).json({ message: "The action could not be located." });
+      }
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Something went wrong updating the action." });
+    }
+  } else {
+    res
+      .status(400)
+      .json({ message: "Please provide a description, id and notes." });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const count = await db.remove(id);
+    if (count) {
+      res.status(200).json(count);
+    } else {
+      res.status(404).json({ message: "The action could not be located." });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Something went wrong trying to delete the action." });
   }
 });
 
