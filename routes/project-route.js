66e5b0c387ad923869b1144ac96e9592302bd6f9
Brandon G-Home <brandon.gardner2@outlook.com>
2019-03-22 10:40:56 -0500

    Project delete route working

--- routes/project-route.js
+++ routes/project-route.js
@@ -58,12 +58,12 @@ router.delete("/:id", async (req, res) => {
     if (count) {
       res.status(200).json(count);
     } else {
-      res.status(404).json({ message: "The action could not be located." });
+      res.status(404).json({ message: "The project could not be located." });
     }
   } catch (error) {
     res
       .status(500)
-      .json({ message: "Something went wrong trying to delete the action." });
+      .json({ message: "Something went wrong trying to delete the project." });
   }
 });
 
